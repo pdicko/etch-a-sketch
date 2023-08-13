3504c43509f1145dae6c475bc19f81cffcd61147
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-12 18:39:36 -0700

    Create functioning hover effect
    Used aspect ratio CSS to make grid square. Implemented javascript to
    change color of each box as they're moused over
    (Step 3 of TOP assignment).

--- script.js
+++ script.js
@@ -3,14 +3,18 @@
 document.addEventListener('DOMContentLoaded', loadGrid);
 
 function loadGrid() {
-
   const  grid = document.querySelector('.grid');
 
   for (i=1; i<=256; i++) {
-
     const newDiv = document.createElement('div');
     newDiv.classList.toggle('box');
-    newDiv.id = `box${i}`;
     grid.appendChild(newDiv);
   }
+
+  grid.addEventListener('mouseover', (e) => {
+    if (e.target.className === 'box') {
+      e.target.classList.toggle('box-filled');
+    }
+  })
 }
+
