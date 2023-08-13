83ff2a6f8e650cb48856097b12fcacb785eb0008
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-13 01:18:46 -0700

    Create Customize Grid modal prompt
    Created a modal dialog that prompts user for a number to create
    a custom sized grid. Completes step 4 of TOP project. Next is the
    'extra credit' steps.

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 
 
-document.addEventListener('DOMContentLoaded', loadGrid);
+document.addEventListener('DOMContentLoaded', () => {
 
-function loadGrid() {
+  const customizeButton = document.querySelector('.customize-button');
+  const modalPrompt = document.querySelector('.modal-prompt');
+
+  customizeButton.addEventListener('click', () => {
+    modalPrompt.showModal();
+  })
+
+  const promptButton = document.querySelector('.prompt-button');
+
+  promptButton.addEventListener('click', () => {
+    const gridSizeInput = document.querySelector('.grid-size-input')
+    const customGridSize = gridSizeInput.value;
+
+    loadGrid(customGridSize);
+  })
+
+  loadGrid(16);
+});
+
+function loadGrid(size) {
+
+  const gridSize = size;
   const  grid = document.querySelector('.grid');
+  grid.innerHTML = '';
+  grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-  for (i=1; i<=256; i++) {
+  for (i=1; i<= (gridSize ** 2); i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.toggle('box');
     grid.appendChild(newDiv);
