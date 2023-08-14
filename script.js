393e29cb9eb5328f5dd3d356203f202ed345b010
pdicko <48698725+pdicko@users.noreply.github.com>
2023-08-14 01:25:16 -0700

    Improve mouseover function
    Mouseover event on a grid square generates a random color with an
    alpha of 10%. Each additional pass increases the alpha by 10% until
    the square reaches a 100% alpha. This incorporates both 'extra credit'
    steps from TOP exercise.

--- script.js
+++ script.js
@@ -31,13 +31,41 @@ function loadGrid(size) {
   for (i=1; i<= (gridSize ** 2); i++) {
     const newDiv = document.createElement('div');
     newDiv.classList.toggle('box');
+    newDiv.id = `box${i}`;
     grid.appendChild(newDiv);
   }
 
   grid.addEventListener('mouseover', (e) => {
     if (e.target.className === 'box') {
-      e.target.classList.toggle('box-filled');
+      const box = e.target;
+      const boxStyle = getComputedStyle(box);
+      const currentBackgroundColor = boxStyle.getPropertyValue('background-color');
+      if (currentBackgroundColor === 'rgb(255, 255, 255)') {
+        box.style.backgroundColor = generateColor();
+      } else {
+        const currentRGB = currentBackgroundColor.slice(0, -5);
+        const currentAlpha = currentBackgroundColor.slice(-4, -1);
+
+        if ((currentAlpha * 10) < 10) {
+          const newAlpha = ((currentAlpha * 10) + 1) / 10;
+          box.style.backgroundColor = `${currentRGB}${newAlpha}`;
+        }
+        
+      }
     }
   })
+}
+
+function generateColor () {
+
+  const red = Math.floor(Math.random() * 255);
+  const green = Math.floor(Math.random() * 255);
+  const blue = Math.floor(Math.random() * 255);
+  const alpha = .1;
+
+  const rgbaValue = `rgba(${red}, ${green}, ${blue}, ${alpha})`;
+
+  return rgbaValue;
+
 }
 
